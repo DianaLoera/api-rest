d590c71cba0f1aaf084a8df4fa29378bfc875853
DianaLoera <grisceldaloera@gmail.com>
2019-11-08 08:43:24 -0600

    creacion de rutas con post, put, get y delete

--- server.js
+++ server.js
@@ -1,50 +1,31 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
+//Parse aplication/x-www-form-urlencode
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Parse formato a aplication/json
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario')
-});
+//Archivo agrupador de rutas
+app.use(require('./server/routes/index'));
 
-app.get('/usuario/:id/:nombre', (req, res) => {
-    let id = req.params.id;
-    let nombre = req.params.nombre;
-    res.json({
-        id,
-        nombre
+//Conexion a la base de datos
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+    },
+    (err, resp) => {
+        if (err) throw err;
+
+        console.log('Base de datos Online');
     });
-});
 
-app.post('/usuario', (req, res) => {
-    let nombre = req.body.nombre;
-    let edad = req.body.edad;
-
-    if (nombre === undefined || edad === undefined) {
-        res.status(400).json({
-            ok: 'false',
-            err: 'Todos los campos son necesarios'
-        })
-    } else {
-        res.json({
-            nombre,
-            edad
-        })
-    }
-});
-
-app.put('/usuario', (req, res) => {
-    res.json('put usuario')
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario')
-});
-
+//Puerto de escucha de la aplicacion
 app.listen(process.env.PORT, () => {
     console.log("escuchando por el puerto: ", process.env.PORT);
 });
